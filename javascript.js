a4a731fe747a46593d939d2708e54268311cb9a0
Aniket-git-hub <63038592+Aniket-git-hub@users.noreply.github.com>
2020-05-01 17:24:45 +0530

    made it more efficient

--- javascript.js
+++ javascript.js
@@ -1,17 +1,12 @@
 const url = "https://api.covid19india.org/data.json";
 const url_2 = "https://api.covid19api.com/summary";
-		
 	// for india
-	
 	// // [for indian states ]
-	window.addEventListener("load", ()=>{
-		     fetch(url).then(res=>{
-			return res.json();
-		}).then(data=>{
+	window.addEventListener("load",()=>fetch(url).then(res=>res.json()).then(data=>{
 			const table = document.getElementById("table"); 
-			const td1 = document.getElementById("conf");
-			const td2 = document.getElementById("rec");
-			const td3 = document.getElementById("dea");
+			const td1 = document.getElementById("conf");// confirm field
+			const td2 = document.getElementById("rec");// recovered field
+			const td3 = document.getElementById("dea");// deaths field
 			let obj = data.statewise;
 			td1.innerText = obj[0].confirmed;
 			td2.innerText = obj[0].recovered;
@@ -25,29 +20,18 @@ const url_2 = "https://api.covid19api.com/summary";
 				text+="<tr><td>"+obj[i].state+"</td><td>"+obj[i].confirmed+"</td><td>"+obj[i].active+"</td><td>"+obj[i].deaths+"</td><td>"+obj[i].recovered+"</tr>";
 			}
 			text+="</table>";
-			table.innerHTML = text;
+			(table) && (table.innerHTML = text);
 		}).catch(error=>{
 			console.log(error);
-		});
-	});
-  
-// [togle function]
-  function togle(){
-  		if (table.style.display == "none") {
-  			table.style.display = "block";
-  		}else{
-  			table.style.display = "none";
-  		}
-  }
+		})
+	);
 // [search function]
   function search(){
   	let input, filter, table, tr, td, i, txtValue;
-
 	  input = document.getElementById("search");
 	  filter = input.value.toUpperCase();
 	  table = document.getElementById("tab");
 	  tr = table.getElementsByTagName("tr");
-
 		  for (i = 0; i < tr.length; i++) {
 		    td = tr[i].getElementsByTagName("td")[0];
 		    if (td) {
@@ -56,16 +40,12 @@ const url_2 = "https://api.covid19api.com/summary";
 		        tr[i].style.display = "";
 		      } else {
 		        tr[i].style.display = "none";
-
     	   }
   	   }
    }
 }
 // [data for world]
-	window.addEventListener("load", ()=>{
-	fetch(url_2).then(res=>{
- 		return res.json();
- 	}).then(data=>{
+	window.addEventListener("load",()=>fetch(url_2).then(res=> res.json()).then(data=>{
 		let global_stat= data.Global;
 		let country_stat = data.Countries;
  		const td1 = document.getElementById("Gconf");
@@ -84,24 +64,11 @@ const url_2 = "https://api.covid19api.com/summary";
 			text+="<tr><td>"+country_stat[i].Country+"</td><td>"+country_stat[i].TotalConfirmed+"</td><td>"+country_stat[i].TotalDeaths+"</td><td>"+country_stat[i].TotalRecovered+"</td></tr>";
 		}
 		text+="</table>";
-		table.innerHTML = text;
- 	}).catch(error=>{
- 		console.log(error);
- 	});
-});
-
+		(table) && (table.innerHTML = text);
+ 	}).catch(error=>console.log(error))
+);
 // [modal fucntion]
-let modal = document.getElementById("modal");
-document.getElementById("modal-envok").addEventListener("click", ()=>{
-
-	modal.style.display = "block";
-
-});
-document.getElementById("close").addEventListener("click" , ()=>{
-	modal.style.display = "none";
-});
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+const modal=document.getElementById("modal");
+document.getElementById("modal-envok").addEventListener("click", ()=> modal.style.display = "block");
+document.getElementById("close").addEventListener("click" , ()=>modal.style.display = "none");
+window.onclick=(event)=>(event.target==modal)&&(modal.style.display="none");
