15699d0c51d40a504a26b871f8c3ccb11e654e89
Aniket-git-hub <63038592+Aniket-git-hub@users.noreply.github.com>
2020-05-02 17:57:14 +0530

    fetching district data

--- javascript.js
+++ javascript.js
@@ -3,21 +3,21 @@ const url_2 = "https://api.covid19api.com/summary";
 	// for india
 	// // [for indian states ]
 	window.addEventListener("load",()=>fetch(url).then(res=>res.json()).then(data=>{
+			let obj = data.statewise;
 			const table = document.getElementById("table"); 
 			const td1 = document.getElementById("conf");// confirm field
 			const td2 = document.getElementById("rec");// recovered field
 			const td3 = document.getElementById("dea");// deaths field
-			let obj = data.statewise;
-			td1.innerText = obj[0].confirmed;
-			td2.innerText = obj[0].recovered;
-			td3.innerText = obj[0].deaths;
+			(td1) && (td1.innerText = obj[0].confirmed);
+			(td2) && (td2.innerText = obj[0].recovered);
+			(td3) && (td3.innerText = obj[0].deaths);
 			let i , text;
 			text= "<input type='text' id='search' onkeyup='search()' title='Search Your State' placeholder='Search States/UTs'>"
 			text+="<table id='tab'>";
 			text+="<caption>All States/UT Stats</caption>";
 			text+="<tr><th>Location</th><th>Total Confirmed</th><th>Active</th><th>Deceased</th><th>Recovered</th></tr>";
 			for (i = 1; i < obj.length; i++) {
-				text+="<tr><td>"+obj[i].state+"</td><td>"+obj[i].confirmed+"</td><td>"+obj[i].active+"</td><td>"+obj[i].deaths+"</td><td>"+obj[i].recovered+"</tr>";
+				text+="<tr onclick='redirect(this)'><td>"+obj[i].state+"</td><td>"+obj[i].confirmed+"</td><td>"+obj[i].active+"</td><td>"+obj[i].deaths+"</td><td>"+obj[i].recovered+"</tr>";
 			}
 			text+="</table>";
 			(table) && (table.innerHTML = text);
@@ -25,6 +25,26 @@ const url_2 = "https://api.covid19api.com/summary";
 			console.log(error);
 		})
 	);
+// [function to display distric data]
+		function redirect(element){
+			const table = document.getElementById("modal_district");
+				table.style.display = 'block';
+			const url_for_distric = "https://api.covid19india.org/state_district_wise.json";
+			const state = element.getElementsByTagName("td")[0].innerHTML;
+			fetch(url_for_distric).then(response=> response.json()).then(raw_data=>{
+				let object = raw_data[state].districtData;
+				let text_data;
+				text= "<input type='text' id='search' onkeyup='search()' title='Search Your Districts' placeholder='Search Districts'>" 
+				text_data= "<table>";
+					text_data+="<caption>Distric Stats</caption>"
+					text_data+="<tr><th>Location</th><th>Total Confirmed</th><th>Active</th><th>Deaths</th><th>Recovered</th></tr>"; 
+				for ( let distric in object) {
+				text_data +="<tr><td>"+distric+"</td><td>"+object[distric].confirmed+"</td><td>"+object[distric].active+"</td><td>"+object[distric].deceased+"</td><td>"+object[distric].recovered+"</td></tr>";
+				}
+					text_data +="</table>";
+				(table) && (table.innerHTML = text_data);
+			}).catch(err=>console.log(err));
+		}
 // [search function]
   function search(){
   	let input, filter, table, tr, td, i, txtValue;
@@ -36,7 +56,7 @@ const url_2 = "https://api.covid19api.com/summary";
 		    td = tr[i].getElementsByTagName("td")[0];
 		    if (td) {
 		      txtValue = td.textContent || td.innerText;
-		      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+		      if (txtValue.toUpperCase().indexOf(filter) > -1){
 		        tr[i].style.display = "";
 		      } else {
 		        tr[i].style.display = "none";
@@ -52,11 +72,11 @@ const url_2 = "https://api.covid19api.com/summary";
 		const td2 = document.getElementById("Grec");
 		const td3 = document.getElementById("Gdea");
 		const table = document.getElementById("table-global");
- 		td1.innerText = global_stat.TotalConfirmed;
- 		td2.innerText = global_stat.TotalRecovered;
- 		td3.innerText = global_stat.TotalDeaths;
+ 		(td1) && (td1.innerText = global_stat.TotalConfirmed);
+ 		(td2) && (td2.innerText = global_stat.TotalRecovered);
+ 		(td3) && (td3.innerText = global_stat.TotalDeaths);
  		let i , text;
-		text= "<input type='text' id='search' onkeyup='search()' title='Search Your State' placeholder='Search States/UTs'>"
+		text= "<input type='text' id='search' onkeyup='search()' title='Search Countries' placeholder='Search Countries'>";
 		text+="<table id='tab'>";
 		text+="<caption>Countries and their stats</caption>";
 		text+="<tr><th>Location</th><th>Confirmed</th><th>Deceased</th><th>Recovered</th></tr>";
@@ -68,7 +88,9 @@ const url_2 = "https://api.covid19api.com/summary";
  	}).catch(error=>console.log(error))
 );
 // [modal fucntion]
+const modal_district = document.getElementById("modal_district");
 const modal=document.getElementById("modal");
 document.getElementById("modal-envok").addEventListener("click", ()=> modal.style.display = "block");
 document.getElementById("close").addEventListener("click" , ()=>modal.style.display = "none");
 window.onclick=(event)=>(event.target==modal)&&(modal.style.display="none");
+window.onclick=(event)=>(event.target==modal_district)&&(modal_district.style.display="none");
